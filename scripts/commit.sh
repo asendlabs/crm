ce6e89c08a24c9c9cf6ebd0a207fbfd13012d3b0
Waris <warisareshi@gmail.com>
2024-10-06 09:00:42 +0530

    fix: minor change to script

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -11,7 +11,7 @@ git add .
 read "commitMessage?Enter your commit message: "
 git commit -m "$commitMessage"
 
-read "runPush?Do you want to push the changes?"
+read "runPush?Do you want to push the changes? (y/n): "
 if [[ "$runPush" == "y" ]]; then
   git push origin main
 fi
