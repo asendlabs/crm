f28b6a5ae417c15ab84097f88d3fca122bd5deb0
Waris <warisareshi@gmail.com>
2024-10-06 08:59:33 +0530

    feat: added a question to invoke push in commit script

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,24 +1,19 @@
 #!/bin/zsh
-
-# Ask if prettier format should be run
 read "runPrettier?Do you want to run 'pnpm prettier:format'? (y/n): "
-
-# If the answer is 'y', run prettier
 if [[ "$runPrettier" == "y" ]]; then
   pnpm prettier:format
 fi
 
-# Step 2: Stage all changes
 git add .
 cd ..
 git add .
 
-# Step 3: Ask for commit message
 read "commitMessage?Enter your commit message: "
-
-# Step 4: Commit with the provided message in quotes
 git commit -m "$commitMessage"
 
-# Step 5: Push the changes
-git push origin main
+read "runPush?Do you want to push the changes?"
+if [[ "$runPush" == "y" ]]; then
+  git push origin main
+fi
+
 exit
