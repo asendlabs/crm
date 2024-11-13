c13791a47129ff32ae2ee6f228171f0b3e16d029
Waris <warisareshi@gmail.com>
2024-11-13 21:32:57 +0530

    feat: neon db

--- database/index.ts
+++ database/index.ts
@@ -1,9 +1,13 @@
 import * as tables from "./tables";
 import * as relations from "./relations";
-
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle as drizzleDevelopment } from "drizzle-orm/postgres-js";
+import {
+  drizzle as drizzleProduction,
+  NeonHttpDriver,
+} from "drizzle-orm/neon-http";
 import { env } from "@/env";
 import postgres from "postgres";
+import { neon, NeonQueryFunction } from "@neondatabase/serverless";
 
 // Determine the database URL based on the environment
 const databaseUrl =
@@ -11,5 +15,15 @@ const databaseUrl =
     ? env.DATABASE_POSTGRES_URL_DEV
     : env.DATABASE_POSTGRES_URL;
 
-export const sql = postgres(databaseUrl);
-export const db = drizzle(sql, { schema: { ...tables, ...relations } });
+// Set up the SQL client depending on the environment
+const sql =
+  env.NODE_ENV === "development" ? postgres(databaseUrl) : neon(databaseUrl);
+
+// Initialize the database with the appropriate drizzle function and schema
+export const db =
+  env.NODE_ENV === "development"
+    ? drizzleDevelopment(sql as any, { schema: { ...tables, ...relations } })
+    : drizzleProduction({
+        client: sql as any,
+        schema: { ...tables, ...relations },
+      });
